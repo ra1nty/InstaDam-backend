72d19bd66c25f23bd33d7e7d38780ef557bb3c52
nishanthrs <nishanth.salinamakki@gmail.com>
2019-02-14 23:52:28 -0600

    Added documentation to each model

--- instadam/models/project.py
+++ instadam/models/project.py
@@ -2,6 +2,17 @@ import datetime as dt
 from ..app import db
 
 class Project(db.Model):
+    """Class Project is a database model to represent a project
+
+    Specifies the full database schema of the table 'projects'
+
+    Attributes:
+        id: unique integer id given to a user (primary key)
+        project_name: unique string that represents name of the project
+        created_by: integer to represent id of user who created project
+        created_at: datetime to represent date at which project was created
+    """
+
     __tablename__ = 'projects'
     id = db.Column(db.Integer, primary_key=True)
     project_name = db.Column(db.String(64), unique=True, nullable=False)
--- instadam/models/project_permissions.py
+++ instadam/models/project_permissions.py
@@ -3,10 +3,28 @@ import datetime as dt
 from ..app import db
 
 class AccessTypeEnum(enum.Enum):
+    """Class AccessTypeEnum is an enum structure to represent the access type of a specific user for a specific project
+
+    Has two possible values: READ_WRITE (given to admin) and READ_ONLY (given to annotators)
+    Return string values
+    """
+
     READ_WRITE = 'rw'
     READ_ONLY = 'r'
 
 class ProjectPermissions(db.Model):
+    """Class User is a database model to represent a project permission
+
+    Specifies the full database schema of the table 'project_permissions'
+    Used to determine which user has access to which project and what kind of access this entails
+
+    Attributes:
+        id: unique integer id given to a permission entry (primary key)
+        project_id: integer project id of permission entry
+        user_id: integer user id that has access to project with project_id
+        access_type: enum type that specifies the level of access
+    """
+
     __tablename__ = 'project_permissions'
     id = db.Column(db.Integer, primary_key=True)
     project_id = db.Column(db.Integer, nullable=False)
