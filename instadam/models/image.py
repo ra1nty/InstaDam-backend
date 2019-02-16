d130661087c21d3cef1ec5fca5e7e3631dd63b8b
nishanthrs <nishanth.salinamakki@gmail.com>
2019-02-16 01:19:53 -0600

    Fixed SQLAlchemy DateTime type

--- instadam/models/image.py
+++ instadam/models/image.py
@@ -19,7 +19,7 @@ class Image(db.Model):
     project_id = db.Column(db.Integer, nullable=False)
     image_name = db.Column(db.String(64), nullable=False)
     added_at = db.Column(
-        db.Datetime, nullable=False, default=dt.datetime.utcnow)
+        db.DateTime, nullable=False, default=dt.datetime.utcnow)
 
     def __repr__(self):
         return '<Image: %r>' % self.image_name
--- instadam/models/project.py
+++ instadam/models/project.py
@@ -19,7 +19,7 @@ class Project(db.Model):
     project_name = db.Column(db.String(64), unique=True, nullable=False)
     created_by = db.Column(db.Integer, nullable=False)
     created_at = db.Column(
-        db.Datetime, nullable=False, default=dt.datetime.utcnow)
+        db.DateTime, nullable=False, default=dt.datetime.utcnow)
 
     def __repr__(self):
         return '<Project: %r>' % self.project_name
