a3ac8a6b471579b158f906a82549989ebcf4eefa
Ra1n <ytang.me@gmail.com>
2019-02-25 17:49:59 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,7 @@
+rsync -e "ssh -o StrictHostKeyChecking=no" -r --delete-after --quiet $TRAVIS_BUILD_DIR $TRAVIS_SSH_USER@$TRAVIS_SSH_HOST:/tmp/$TRAVIS_BUILD_ID;
+ssh -o StrictHostKeyChecking=no $TRAVIS_SSH_USER@$TRAVIS_SSH_HOST:/tmp/$TRAVIS_BUILD_ID;
+rm -rf ~/instadam
+cp -r $(pwd) ~/instadam
+cd ~/instadam
 docker-compose up -d app --build
+exit
