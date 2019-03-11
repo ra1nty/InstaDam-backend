a8772cb44b2c431cf2cb12ddc166a09f604db76c
Daddy <ytang.me@gmail.com>
2019-03-11 16:58:42 -0500

    add CD

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+rsync -e "ssh -o StrictHostKeyChecking=no" -r --delete-after --quiet $TRAVIS_BUILD_DIR $TRAVIS_SSH_USER@$TRAVIS_SSH_HOST:/tmp/$TRAVIS_BUILD_ID;
+ssh -o StrictHostKeyChecking=no $TRAVIS_SSH_USER@$TRAVIS_SSH_HOST -t\
+ "docker-compose down;\
+cd /tmp/${TRAVIS_BUILD_ID};\
+sudo rm -rf ~/instadam;\
+mkdir -p ~/instadam;\
+sudo mv /tmp/${TRAVIS_BUILD_ID}/InstaDam-backend ~/instadam;\
+cd ~/instadam/InstaDam-backend;\
+source ~/.bashrc;\
+docker-compose up --build"
