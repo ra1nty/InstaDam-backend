745e39836b853e9cdfcafea3bca61eaf7a206eb6
Daddy <ytang.me@gmail.com>
2019-03-11 18:25:06 -0500

    fix cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rsync -e "ssh -o StrictHostKeyChecking=no" -r --delete-after --quiet $TRAVIS_BUILD_DIR $TRAVIS_SSH_USER@$TRAVIS_SSH_HOST:/tmp/$TRAVIS_BUILD_ID;
-ssh -o StrictHostKeyChecking=no $TRAVIS_SSH_USER@$TRAVIS_SSH_HOST -t "source ~/.bashrc;\
+ssh -o StrictHostKeyChecking=no $TRAVIS_SSH_USER@$TRAVIS_SSH_HOST -t "sudo source ~/.bashrc;\
 cd ~/InstaDam-backend;\
 sudo docker-compose down;\
 cd /tmp/${TRAVIS_BUILD_ID};\
