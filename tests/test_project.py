da88bb694e4e7e7e7c9a315acb54466c8d8e1cc0
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-03-24 17:05:44 -0500

    Added tests

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,6 +1,8 @@
 import pytest
+
 from instadam.app import create_app, db
 from instadam.models.project import Project
+from instadam.models.project_permission import AccessTypeEnum, ProjectPermission
 from instadam.models.user import PrivilegesEnum, User
 from tests.conftest import TEST_MODE
 
@@ -14,6 +16,7 @@ FAIL_PROJECT_NAME = "fail project"
 DUPLICATE_PROJECT_NAME = "duplicate project"
 
 PROJECT_ENDPOINT = '/project'
+LIST_PROJECT_ENDPOINT = '/projects'
 
 
 @pytest.fixture(scope="module")
@@ -71,7 +74,7 @@ def test_create_project_success(client):
 
 def test_create_project_not_logged_in(client):
     response = client.post(PROJECT_ENDPOINT,
-                          json={'project_name': FAIL_PROJECT_NAME})
+                           json={'project_name': FAIL_PROJECT_NAME})
     assert response.status_code == 401
     res = response.get_json()
     assert 'msg' in res
@@ -109,3 +112,69 @@ def test_create_project_duplicate_project_name(client):
 
 def test_create_project_illegal_project_name(client):
     pass
+
+
+@pytest.fixture
+def get_project_fixture():
+    app = create_app(TEST_MODE)
+    with app.app_context():
+        db.create_all()
+
+        user = User(username=ADMIN_USERNAME,
+                    email='success@test_project.com',
+                    privileges=PrivilegesEnum.ADMIN)
+        user.set_password(ADMIN_PWD)
+        db.session.add(user)
+
+        db.session.flush()
+        db.session.commit()
+
+        project = Project(project_name='test1', created_by=user.id)
+        db.session.add(project)
+        db.session.flush()
+        db.session.commit()
+
+        permission = ProjectPermission(access_type=AccessTypeEnum.READ_WRITE)
+        user.project_permissions.append(permission)
+        project.permissions.append(permission)
+
+        user = User(username=ANNOTATOR_USERNAME,
+                    email='no_privilege@test_project.com',
+                    privileges=PrivilegesEnum.ANNOTATOR)
+        user.set_password(ANNOTATOR_PWD)
+
+        db.session.add(user)
+        db.session.flush()
+        db.session.commit()
+
+        permission = ProjectPermission(access_type=AccessTypeEnum.READ_ONLY)
+        user.project_permissions.append(permission)
+        project.permissions.append(permission)
+
+        db.session.flush()
+        db.session.commit()
+
+    client = app.test_client()
+    yield client
+
+
+def test_list_projects(get_project_fixture):
+    token = login(get_project_fixture, ANNOTATOR_USERNAME, ANNOTATOR_PWD)
+    response = get_project_fixture.get(
+        LIST_PROJECT_ENDPOINT,
+        headers={'Authorization': 'Bearer %s' % token})
+    assert response.status_code == 200
+    json = response.get_json()
+    assert 1 == len(json)
+    assert 'test1' == json[0]['name']
+    assert not json[0]['is_admin']
+
+    token = login(get_project_fixture, ADMIN_USERNAME, ADMIN_PWD)
+    response = get_project_fixture.get(
+        LIST_PROJECT_ENDPOINT,
+        headers={'Authorization': 'Bearer %s' % token})
+    assert response.status_code == 200
+    json = response.get_json()
+    assert 1 == len(json)
+    assert 'test1' == json[0]['name']
+    assert json[0]['is_admin']
