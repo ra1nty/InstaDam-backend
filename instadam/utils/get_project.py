8c6073d98fef7eda033dc7d16d76c42b22fdbf10
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-03-31 15:27:43 -0500

    Added get thumbnail

--- instadam/utils/get_project.py
+++ instadam/utils/get_project.py
@@ -1,8 +1,8 @@
 from flask_jwt_extended import get_jwt_identity
 from werkzeug.exceptions import abort
 
-from instadam.models.project_permission import ProjectPermission, AccessTypeEnum
-from instadam.models.user import User, PrivilegesEnum
+from instadam.models.project_permission import AccessTypeEnum, ProjectPermission
+from instadam.models.user import PrivilegesEnum, User
 
 
 def maybe_get_project(project_id):
@@ -16,4 +16,18 @@ def maybe_get_project(project_id):
         access_type=AccessTypeEnum.READ_WRITE).first()
     if permission is None:
         abort(401, 'User does not have permission to add image to this project')
+    return permission.project
+
+
+def maybe_get_project_read_only(project_id):
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(username=current_user).first()
+    permission = ProjectPermission.query.filter(
+        (ProjectPermission.user_id == user.id)
+        & (ProjectPermission.project_id == project_id)).filter(
+        (ProjectPermission.access_type == AccessTypeEnum.READ_ONLY)
+        | (ProjectPermission.access_type == AccessTypeEnum.READ_WRITE)).first()
+    if permission is None:
+        abort(401,
+              'User does not have permission to read images of this project')
     return permission.project
