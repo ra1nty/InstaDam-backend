66f670f343c0dd09e8c739b742fb8bdebbbaf2c6
nishanthrs <nishanth.salinamakki@gmail.com>
2019-03-31 17:52:13 -0500

    Fixed imports

--- instadam/search_users.py
+++ instadam/search_users.py
@@ -1,7 +1,7 @@
 """Module related to searching for users
 """
 from flask import Blueprint, abort, jsonify, request
-from flask_jwt_extended import (jwt_required)
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from instadam.app import db
 from sqlalchemy.exc import IntegrityError
 
