05246839cd18610ddfa2d39bb73460d610d67194
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-04-01 23:52:36 -0500

    Fixing deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
-rsync -e "ssh -o StrictHostKeyChecking=no" -r --delete-after --quiet $TRAVIS_BUILD_DIR $TRAVIS_SSH_USER@$TRAVIS_SSH_HOST:/tmp/$TRAVIS_BUILD_ID;
-ssh -o StrictHostKeyChecking=no $TRAVIS_SSH_USER@$TRAVIS_SSH_HOST -t "sudo source ~/.bashrc;\
-cd ~/InstaDam-backend;\
-sudo docker-compose down;\
-cd /tmp/${TRAVIS_BUILD_ID};\
-sudo rm -rf ~/InstaDam-backend;\
-sudo mv /tmp/${TRAVIS_BUILD_ID}/InstaDam-backend ~/;\
-cd ~/InstaDam-backend;\
-sudo docker-compose --verbose up --build&"
+rsync -e "ssh -o StrictHostKeyChecking=no" -r --delete-after --quiet $TRAVIS_BUILD_DIR instadam@23.100.23.94:/home/instadam;
+ssh -o StrictHostKeyChecking=no instadam@23.100.23.94 -t "sudo systemctl restart instadam-backendd"
