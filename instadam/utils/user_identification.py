ee109ae1e98141fb00c778bc5720b091ff62c40d
SamYang95 <yangwx01@yeah.net>
2019-04-01 20:18:30 -0500

    apply admin_checking to get_project.py maybe_get_project

--- instadam/utils/user_identification.py
+++ instadam/utils/user_identification.py
@@ -5,5 +5,5 @@ from flask import abort
 def check_user_admin_privilege(username):
     user = User.query.filter_by(username=username).first()
     if user.privileges != PrivilegesEnum.ADMIN:
-        abort(403, 'Logged in user is not admin')
+        abort(401, 'Logged in user is not an admin.')
     return user
